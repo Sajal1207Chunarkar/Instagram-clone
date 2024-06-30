aa258004f203e1d27cfd84b353d4e46f96e9b6e1
Sajal1207Chunarkar <sajal1207chunarkar@gmail.com>
2024-06-30 11:36:36 +0530

    Instagram first commit

---
+++ instagram-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    
+      <div>
+        <h1>Instagram clone</h1>
+      </div>
+      
+    
+  )
+}
+
+export default App
